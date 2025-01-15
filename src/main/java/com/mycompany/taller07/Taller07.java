0e443805789387a4ed871afcc8a0538360757218
jorssanc <jorssanc@espol.edu.ec>
2025-01-15 16:09:04 -0500

    inicio

---
+++ src/main/java/com/mycompany/taller07/Taller07.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller07;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Taller07 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
